4e48b12a53d42c7d82dc92ff0d5d8585d19cb121
nspektor <nellie.spektor@gmail.com>
2016-03-03 10:40:45 -0500

    draw.c done (not working probably though)

--- draw.c
+++ draw.c
@@ -36,94 +36,8 @@ should use add_point
 void add_edge( struct matrix * points, 
 	       int x0, int y0, int z0, 
 	       int x1, int y1, int z1) {
-  
-  int x, y, d, dx, dy, z;
-  z = 1;
-  x = x0;
-  y = y0;
-  //swap points so we're always draing left to right
-  if ( x0 > x1 ) {
-    x = x1;
-    y = y1;
-    x1 = x0;
-    y1 = y0;
-  }
-  //need to know dx and dy for this version
-  dx = (x1 - x) * 2;
-  dy = (y1 - y) * 2;
-  //positive slope: Octants 1, 2 (5 and 6)
-  if ( dy > 0 ) {
-    //slope < 1: Octant 1 (5)
-    if ( dx > dy ) {
-      d = dy - ( dx / 2 );
-      while ( x <= x1 ) {
-	//ADD X AND Y TO THE MATRIX
-	add_point(points,x, y, z);
-	if ( d < 0 ) {
-	  x = x + 1;
-	  d = d + dy;
-	}
-	else {
-	  x = x + 1;
-	  y = y + 1;
-	  d = d + dy - dx;
-	}
-      }
-    }
-    //slope > 1: Octant 2 (6)
-    else {
-      d = ( dy / 2 ) - dx;
-      while ( y <= y1 ) {
-	//ADD X AND Y TO THE MATRIX
-	add_point(points,x, y, z);
-	if ( d > 0 ) {
-	  y = y + 1;
-	  d = d - dx;
-	}
-	else {
-	  y = y + 1;
-	  x = x + 1;
-	  d = d + dy - dx;
-	}
-      }
-    }
-  }
-  //negative slope: Octants 7, 8 (3 and 4)
-  else {
-    //slope > -1: Octant 8 (4)
-    if ( dx > abs(dy) ) {
-      d = dy + ( dx / 2 );
-      while ( x <= x1 ) {
-	//ADD X AND Y TO THE MATRIX
-	if ( d > 0 ) {
-	  x = x + 1;
-	  d = d + dy;
-	}
-	else {
-	  x = x + 1;
-	  y = y - 1;
-	  d = d + dy + dx;
-	}
-      }
-    }
-    //slope < -1: Octant 7 (3)
-    else {
-      d = (dy / 2) + dx;
-      while ( y >= y1 ) {
-	//ADD X AND Y TO THE MATRIX
-	add_point(points,x, y, z);
-	if ( d < 0 ) {
-	  y = y - 1;
-	  d = d + dx;
-	}
-	else {
-	  y = y - 1;
-	  x = x + 1;
-	  d = d + dy + dx;
-	}
-      }
-    }
-  }
+  add_point(points, x0, y0, z0);
+  add_point(points, x1, y1, z1);
 }
 
  
@@ -137,6 +51,10 @@ Go through points 2 at a time and call draw_line to add that line
 to the screen
 ====================*/
 void draw_lines( struct matrix * points, screen s, color c) {
+  int i;
+  for(i=0;i<(points->lastcol)-1;i++){
+    draw_line(points->points[i][0], points->points[i][1], points->points[i+1][0], points->points[i+1][1], s, c);  
+  }
 }
 
 
